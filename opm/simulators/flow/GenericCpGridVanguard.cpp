3eb6af22ad48ee59d5872b0adb55b83db68c8b9d
Vegard Kippe <vkip@equinor.com>
2024-12-19 15:16:30 +0100

    Ensure proper RFT output for inactive wells also for partition-method=3

--- opm/simulators/flow/GenericCpGridVanguard.cpp
+++ opm/simulators/flow/GenericCpGridVanguard.cpp
@@ -214,7 +214,11 @@ doLoadBalance_(const Dune::EdgeWeightMethod             edgeWeightsMethod,
         }
 
         // Add inactive wells to all ranks with connections (not solved, so OK even without distributed wells)
+        // Also ensure all wells are added to rank 0 for output (required for partition methods where rank 0 does not have all cells)
         if (split_inactive) {
+            const auto& comm = this->grid_->comm();
+            const bool ioRank = comm.rank() == 0;
+
             std::unordered_set<unsigned> cellOnRank;
             const auto& global_cells = this->grid_->globalCell();
             for (const auto cell : global_cells) cellOnRank.insert(cell);
@@ -225,7 +229,7 @@ doLoadBalance_(const Dune::EdgeWeightMethod             edgeWeightsMethod,
             for (const auto& well_name : inactive_well_names) {
                 const auto& well = schedule.getWell(well_name, schedule.size()-1);
                 for (const auto& conn: well.getConnections()) {
-                    if (cellOnRank.count(conn.global_index()) > 0) {
+                    if (ioRank || cellOnRank.count(conn.global_index()) > 0) {
                         well_on_rank[well_idx] = 1;
                         parallelWells.emplace_back(well_name, true);
                         break;
@@ -236,7 +240,6 @@ doLoadBalance_(const Dune::EdgeWeightMethod             edgeWeightsMethod,
             }
 
             // Provide information message
-            const auto& comm = this->grid_->comm();
             const auto nranks = comm.size();
             // // values from rank i will be at indices i*num_wells, i*num_wells + 1, ..., (i+1) * num_wells -1
             std::vector<int> well_on_rank_global(num_wells * nranks, 0);
