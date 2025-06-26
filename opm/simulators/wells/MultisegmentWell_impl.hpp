3937556ca8bd6cc917dccd6c7481452fefdb1b88
Vegard Kippe <vkip@equinor.com>
2025-06-26 12:28:21 +0200

    Try to avoid open/shut oscillations in producers under THP control

--- opm/simulators/wells/MultisegmentWell_impl.hpp
+++ opm/simulators/wells/MultisegmentWell_impl.hpp
@@ -1657,7 +1657,7 @@ namespace Opm
             ++its_since_last_switch;
             if (allow_switching && its_since_last_switch >= min_its_after_switch && status_switch_count < max_status_switch){
                 const Scalar wqTotal = this->primary_variables_.getWQTotal().value();
-                bool changed = this->updateWellControlAndStatusLocalIteration(simulator, well_state, group_state,
+                bool changed = this->updateWellControlAndStatusLocalIteration(simulator, it, well_state, group_state,
                                                                               inj_controls, prod_controls, wqTotal,
                                                                               deferred_logger, fixed_control,
                                                                               fixed_status);
